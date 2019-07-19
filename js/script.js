773782c27e1b847510e5a39e2c231806fc7093dd
alex-muliande <alexmuliande@gmail.com>
2019-07-19 19:54:43 +0300

    add links of jQuery and java-script as well as foulders of css and java script.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// $(document).ready(function(){
+
+//     $("#design-pic").click(function(){
+//       $(".design-a").hide(1500).toggle();
+//       $("#design-pic").slideUp();
+    
+//     });
+//     $(".design-a").click(function(){
+//       $("#design-pic").slideDown();
+//       $(".design-a").hide();
+//     })
+    
+    
+//     $("#develop-pic").click(function(){
+//        $(".develop-a").hide(1500).toggle();
+//        $("#develop-pic").slideUp();
+//      });
+//      $(".develop-a").click(function(){
+//        $("#develop-pic").slideDown();
+//        $(".develop-a").hide();
+//      })
+    
+//      $("#prod-pic").click(function(){
+//         $(".prod-a").toggle();
+//         $("#prod-pic").toggle();
+//       });
+//       $(".prod-a").click(function(){
+//         $("#prod-pic").show();
+//         $(".prod-a").hide();
+//       })
+    
+//      });
